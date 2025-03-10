c975e84eaf113bf3369785c979c3ecb445087548
Alex-Khavin <alexandr_havin@yahoo.com>
2025-03-10 07:48:16 +0000

    Deploying to gh-pages from @ Alex-Khavin/goit-js-hw-10@f45b92ccd5bba1308134ca97abd148f54a28e4a8 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");document.querySelector("[name='delay']");t.addEventListener("submit",r);function r(e){return e.preventDefault(),new Promise((o,m)=>{})}
+//# sourceMappingURL=2-snackbar.js.map
