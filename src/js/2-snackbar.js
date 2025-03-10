d3beba1ec58604e3366c9dd4458669d981c098f4
Alex Khavin <alexandr_havin@yahoo.com>
2025-03-10 17:53:57 +0200

    ready hw-10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,113 +1,55 @@
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-// const images = [
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-//     description: 'Container Haulage Freight',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-//     description: 'Aerial Beach View',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-//     description: 'Flower Blooms',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-//     description: 'Alpine Mountains',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-//     description: 'Mountain Lake Sailing',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-//     description: 'Alpine Spring Meadows',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-//     description: 'Nature Landscape',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-//     description: 'Lighthouse Coast Sea',
-//   },
-// ];
-
-// const galleryList = document.querySelector(".gallery");
-
-// function markupImg(cards) {
-//   return cards
-//   .map(card => {
-//     return `<li class="gallery-item">
-//     <a class="gallery-link" href="${card.original}">
-//     <img
-//     class="gallery-image"
-//     src="${card.preview}"
-//     alt="${card.description}"
-//     />
-//     </a>
-//     </li>`;
-//   })
-//   .join("");
-// }
-
-// galleryList.insertAdjacentHTML("beforeend", markupImg(images));
-
-// let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
-
-//Видалив атрибут title
-
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+let delay;
+let state = "fulfilled";
+
 const formSubmit = document.querySelector(".form");
 const inputEl = document.querySelector("[name='delay']");
+const radioEl = document.querySelectorAll("[name='state']");
 
-formSubmit.addEventListener("submit", handlerSubmit);
-// inputEl.addEventListener("input", )
-
-function handlerSubmit(event) {
+formSubmit.addEventListener("submit", event => {
   event.preventDefault();
+  
+  createPromise(delay, state)
+    .then(message => iziToast.show({
+      message: `${message}`,
+      position: 'topRight',
+      color: 'green',
+    }))
+    .catch(error => iziToast.show({
+      message: `${error}`,
+      position: 'topRight',
+      color: 'red',
+    }));
+});
+
+inputEl.addEventListener("input", handlerInput);
+
+function handlerInput(event) {
+  delay = Number(event.target.value);
+};
+
+radioEl.forEach(radio => {
+  radio.addEventListener("click", event => {
+    state = event.target.value;
+  });
+});
+
+function createPromise(delay, state) {
 
   return new Promise((resolve, reject) => {
 
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(`✅ Fulfilled promise in ${delay} ms`);
+      }
+      reject(`❌ Rejected promise in ${delay} ms`);
+      
+    }, delay)
   })
 }
+
 
 
 
